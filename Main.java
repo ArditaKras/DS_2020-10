974886b74f02940ea183770f963be1c5564409e6
ArditaKras <58491146+ArditaKras@users.noreply.github.com>
2020-03-29 01:02:03 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,18 @@ import javax.sound.sampled.*;
 
 public class Main{
 		public static void main(String[] args)throws  ArrayIndexOutOfBoundsException, IOException, LineUnavailableException, InterruptedException{
+		
+			
 		Morse M = new Morse();
 		Tap T = new Tap();
-
+		Vigenere V=new Vigenere();
+                //how to use arguments in console
 		if(args.length > 4 || args.length < 3 || args.length == 0){
 			System.out.println("\n\t\tProgrami pranon deri ne 4 argumente jo me shume!\t\t\n");
 			System.out.println("~Perdor morse-code encode | decode <text>\n" + "~Perdor tap-code encode | decode <text>\n" + "~Perdor vigenere-code encrypt | decrypt <text>\n");
 			System.exit(0);
 		}
+			// morse-code
 		if("morse-code".equals(args[0])){
 			if("encode".equals(args[1]))
 				System.out.println(M.encode(args[2]));
@@ -20,11 +24,31 @@ public class Main{
 		else if("beep".equals(args[1]))
 			M.BeepAudio(args[2]);
 			}
+			//tap-code
 		if("tap-code".equals(args[0])){
 			if("encode".equals(args[1]))
 					T.encode(args[2]);
 			else if("decode".equals(args[1]))
 					T.decode(args[2]);
 			}
+			//vigenere-code
+				if("vigenere-cipher".equals(args[0])){
+					if("encrypt".equals(args[1])){
+						String key = args[2];
+						String message = args[3];
+						String encrypted = V.encrypt(key,message);
+						System.out.println(encrypted);
+						
+					}
+					else if("decrypt".equals(args[1])){
+						String key = args[2];
+						String message = args[3];
+						String decrypted = V.decrypt(key,message);
+						System.out.println(decrypted);
+					}
+					
+				}
 		}
+
+
 }
