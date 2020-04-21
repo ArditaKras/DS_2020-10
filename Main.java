bdf0d6f3c5c12f097b329f8414e33e436f5377fe
endrit <58190531+endritbalaj@users.noreply.github.com>
2020-04-21 23:32:50 +0200

    create-user

--- Main.java
+++ Main.java
@@ -1,54 +1,87 @@
 import java.util.*;
 import java.io.*;
 import javax.sound.sampled.*;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
 
 public class Main{
 		public static void main(String[] args)throws  ArrayIndexOutOfBoundsException, IOException, LineUnavailableException, InterruptedException{
 		
-			
-		Morse M = new Morse();
-		Tap T = new Tap();
-		Vigenere V=new Vigenere();
-                //how to use arguments in console
-		if(args.length > 4 || args.length < 3 || args.length == 0){
-			System.out.println("\n\t\tProgrami pranon deri ne 4 argumente jo me shume!\t\t\n");
-			System.out.println("~Perdor morse-code encode | decode <text>\n" + "~Perdor tap-code encode | decode <text>\n" + "~Perdor vigenere-code encrypt | decrypt <text>\n");
-			System.exit(0);
-		}
-			// morse-code
-		if("morse-code".equals(args[0])){
-			if("encode".equals(args[1]))
-				System.out.println(M.encode(args[2]));
-		else if("decode".equals(args[1]))
-			M.decode(args[2]);
-		else if("beep".equals(args[1]))
-			M.BeepAudio(args[2]);
-			}
-			//tap-code
-		if("tap-code".equals(args[0])){
-			if("encode".equals(args[1]))
-					T.encode(args[2]);
-			else if("decode".equals(args[1]))
-					T.decode(args[2]);
-			}
-			//vigenere-code
-				if("vigenere-cipher".equals(args[0])){
-					if("encrypt".equals(args[1])){
-						String key = args[2];
-						String message = args[3];
-						String encrypted = V.encrypt(key,message);
-						System.out.println(encrypted);
-						
-					}
-					else if("decrypt".equals(args[1])){
-						String key = args[2];
-						String message = args[3];
-						String decrypted = V.decrypt(key,message);
-						System.out.println(decrypted);
-					}
-					
-				}
-		}
 
+			GenerateKeys generate;
+			
+		
+			Morse M = new Morse();
+		
+			
+			
+	                //how to use arguments in console
+			if(args.length > 4 || args.length < 2 || args.length == 0){
+				System.out.println("\n\t\tProgrami pranon deri ne 4 argumente jo me shume!\t\t\n");
+				System.out.println("~Perdor morse-code encode | decode <text>\n" + "~Perdor tap-code encode | decode <text>\n" + "~Perdor vigenere-code encrypt | decrypt <text>\n");
+				System.exit(0);
+			}
+			
+			
+				
+			if("morse-code".equals(args[0])){
+				if("encode".equals(args[1]))
+					System.out.println(M.encode(args[2]));
+			else if("decode".equals(args[1]))
+				M.decode(args[2]);
+			else if("beep".equals(args[1]))
+				M.BeepAudio(args[2]);
+				}
+				//tap-code
+			if("tap-code".equals(args[0])){
+				if("encode".equals(args[1]))
+						Tap.encode(args[2]);
+				else if("decode".equals(args[1]))
+						Tap.decode(args[2]);
+				}
+			//vigenere-code
+			if("vigenere-cipher".equals(args[0])){
+				if("encrypt".equals(args[1])){
+					String key = args[2];
+					String message = args[3];
+					String encrypted = Vigenere.encrypt(key,message);
+					System.out.println(encrypted);
+						
+				}
+				else if("decrypt".equals(args[1])){
+					String key = args[2];
+					String message = args[3];
+					String decrypted = Vigenere.decrypt(key,message);
+					System.out.println(decrypted);
+				}
+					
+			}
+			
+			if("create-user".equals(args[0]))
+			{
+				try {
+					generate = new GenerateKeys(1024);
+					generate.createKeys();
+					String emri = args[1];
+					String emri1 = "keys/" + emri + ".xml";
+					String emri2 = "keys/" + emri + ".pub.xml";
+					generate.writeToFile(emri1 , generate.getPublicKey().getEncoded());
+					generate.writeToFile(emri2 , generate.getPrivateKey().getEncoded());
+					System.out.println("Eshte krijuar celesi privat " + emri1);
+					System.out.println("Eshte krijuar celesi publik " + emri2);
+					}
+				catch (NoSuchAlgorithmException | NoSuchProviderException e) 
+				{
+					System.err.println(e.getMessage());
+				} 
+				catch (IOException e)
+				{
+					System.err.println(e.getMessage());
+				}
+			}
+			
+	}
 
 }
