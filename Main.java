43c1d3445894c3bbd6eb81c837c36d1e8f22dd7c
endrit <58190531+endritbalaj@users.noreply.github.com>
2020-04-23 16:45:43 +0200

    cmd delete-user

--- Main.java
+++ Main.java
@@ -97,25 +97,7 @@ public class Main{
 			if("delete-user".equals(args[0]))
 			{
 				String emri = args[1];
-				String emri1 = "keys/" + emri + ".xml";
-				String emri2 = "keys/" + emri + ".pub.xml";
-		        File file1 = new File(emri1); 
-		        File file2 = new File(emri2); 
-		          
-		        if(file1.delete()) 
-		        { 
-		            System.out.println("Eshte larguar celesi privat '" + emri1 + "'"); 
-		        } 
-		        
-		        if(file2.delete()) 
-		        { 
-		            System.out.println("Eshte larguar celesi publik '" + emri2 + "'"); 
-		        } 
-		        else
-		        { 
-		            System.out.println("Gabim: Celesi '" + emri + "' nuk egziston."); 
-		        } 
-		        
+				DeleteUser.find(emri);
 			}
 		
 	}
