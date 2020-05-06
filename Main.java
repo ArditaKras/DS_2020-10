b249d9a0c1757026f22708d42062e26d8daac7d0
ArditaKras <58491146+ArditaKras@users.noreply.github.com>
2020-05-06 20:39:10 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,153 +1,108 @@
-
-import javax.crypto.Cipher;
 import javax.sound.sampled.*;
-
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.Key;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
-import java.security.Security;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
+public class Main {
+  public static void main(String[] args)throws IOException, LineUnavailableException, InterruptedException, 
+                            ArrayIndexOutOfBoundsException, NoSuchAlgorithmException, InvalidKeyException, 
+                            NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        Morse M = new Morse();
+        Create C= new Create();
+        Delete D=  new Delete();
+        Export E=  new Export();
+        Import I=  new Import();
+        Write  W=  new Write();
+        Read   R=  new Read();
+        
+        C.writeToFile("RSA/publicKey", C.getPublicKey().getEncoded());
+        C.writeToFile("RSA/privateKey", C.getPrivateKey().getEncoded());
+        
+        if(args.length == 0 || args.length > 5){
 
-public class Main{
-		public static void main(String[] args)throws ArrayIndexOutOfBoundsException, IOException, LineUnavailableException, InterruptedException{
-		
+           System.out.println("\n\t\tProgrami pranon deri ne 5 argumente!\t\t\n");
+                System.out.println("~Perdor morse-code encode | decode <text>\n" + 
+                    "~Perdor tap-code encode | decode <text>\n" + 
+                    "~Perdor vigenere-code encrypt | decrypt <text>\n");
+                System.exit(0);
+            }
+            
+        if("morse-code".equals(args[0])){
+                if("encode".equals(args[1]))
+                    System.out.println(M.encode(args[2]));
+            else if("decode".equals(args[1]))
+                M.decode(args[2]);
+            else if("beep".equals(args[1]))
+                M.BeepAudio(args[2]);
+                }
+                //tap-code
+            if("tap-code".equals(args[0])){
+                if("encode".equals(args[1]))
+                        Tap.encode(args[2]);
+                else if("decode".equals(args[1]))
+                        Tap.decode(args[2]);
+                }
+            //vigenere-code
+            if("vigenere-cipher".equals(args[0])){
+                if("encrypt".equals(args[1])){
+                    String key = args[2];
+                    String message = args[3];
+                    String encrypted = Vigenere.encrypt(key,message);
+                    System.out.println(encrypted);
+                        
+                }
+                else if("decrypt".equals(args[1])){
+                    String key = args[2];
+                    String message = args[3];
+                    String decrypted = Vigenere.decrypt(key,message);
+                    System.out.println(decrypted);
+                }
+                    
+            }
+        
+        // Faza II
+        
+        if(args[0].equals("create-user")) {
+            if(args[1].matches("^[a-zA-Z0-9._]+")) {
+                C.CreateUser(args[1]);
+            }
+            else {
+                System.out.println("Keni shenuar gabim. Lejohen vetem shkronjat,numrat dhe . _ ] +");
+            }
+        }
+           else if(args[0].equals("delete-user")) {
+            D.Delete(args[1]);
+        }
+        else if(args[0].equals("export-key")) {
+            if(args.length == 3) {
+                E.Export(args[1], args[2]);
+            }
+            else {
+                E.Export(args[1], args[2],args[3]);
+            }
+        }
+         else if(args[0].equals("import-key")) {
+                I.Import(args[1], args[2]);
 
-			CreateUser generate;
-			
-		
-			Morse M = new Morse();
-		
-			
-			
-	                //how to use arguments in console
-			if(args.length > 4 || args.length < 2 || args.length == 0){
-				System.out.println("\n\t\tProgrami pranon deri ne 4 argumente jo me shume!\t\t\n");
-				System.out.println("~Perdor morse-code encode | decode <text>\n" + "~Perdor tap-code encode | decode <text>\n" + "~Perdor vigenere-code encrypt | decrypt <text>\n");
-				System.exit(0);
-			}
-			
-			
-			
-			if("morse-code".equals(args[0])){
-				if("encode".equals(args[1]))
-					System.out.println(M.encode(args[2]));
-			else if("decode".equals(args[1]))
-				M.decode(args[2]);
-			else if("beep".equals(args[1]))
-				M.BeepAudio(args[2]);
-				}
-				//tap-code
-			if("tap-code".equals(args[0])){
-				if("encode".equals(args[1]))
-						Tap.encode(args[2]);
-				else if("decode".equals(args[1]))
-						Tap.decode(args[2]);
-				}
-			//vigenere-code
-			if("vigenere-cipher".equals(args[0])){
-				if("encrypt".equals(args[1])){
-					String key = args[2];
-					String message = args[3];
-					String encrypted = Vigenere.encrypt(key,message);
-					System.out.println(encrypted);
-						
-				}
-				else if("decrypt".equals(args[1])){
-					String key = args[2];
-					String message = args[3];
-					String decrypted = Vigenere.decrypt(key,message);
-					System.out.println(decrypted);
-				}
-					
-			}
-			
-			if("create-user".equals(args[0]))
-			{
-				try {
-					
-					String emri = args[1];
-					String emri1 = "keys/" + emri + ".xml";
-					String emri2 = "keys/" + emri + ".pub.xml";
-					
-					Path path = Paths.get(emri1);
-					
-					if (Files.exists(path)) {
-					  System.out.println("Gabim: Celesi '" + emri + "' egziston paraprakisht. ");
-					}
-					if (Files.notExists(path)) {
-						generate = new CreateUser(1024);
-						generate.createKeys();
-						generate.writeToFile(emri1 , generate.getPublicKey().getEncoded());
-						generate.writeToFile(emri2 , generate.getPrivateKey().getEncoded());
-						System.out.println("Eshte krijuar celesi privat '" + emri1 + "'");
-						System.out.println("Eshte krijuar celesi publik '" + emri2 + "'");
-					}
-					
-					}
-				catch (NoSuchAlgorithmException | NoSuchProviderException e) 
-				{
-					System.err.println(e.getMessage());
-				} 
-				catch (IOException e)
-				{
-					System.err.println(e.getMessage());
-				}
-			}
-			if("delete-user".equals(args[0]))
-			{
-				String emri = args[1];
-				DeleteUser.find(emri);
-			}
-			if("write-message".equals(args[0]))
-			{
-				String name = args[1];
-				String em = "keys/" + name + ".pub.xml";
-				Path path = Paths.get(em);
-				 
-					
-				if(args.length < 4)
-				{
-					if (Files.exists(path))
-					{
-						
-						String message = args[2];
-						
-						WriteMessage.Write(name,message);
-						}
-					else if (Files.notExists(path))
-					{
-						System.out.println("Gabim: Celesi '" + name + "' nuk egziston paraprakisht. ");
-					}
-				}
-				else if(args.length > 3)
-				{
-					if (Files.exists(path))
-					{
-						String message = args[2];
-						String file = args[3];
-						WriteMessage.Write1(name,message,file);
-					}
-					else if (Files.notExists(path))
-					{
-						System.out.println("Gabim: Celesi '" + name + "' nuk egziston paraprakisht. ");
-					}
-				}	
-			
-				
-			}
-		
-		
-		
-	}
-
-
+        }
+           else if(args[0].equals("write-message")) {
+            if(args.length == 3) {
+                W.Write(args[1], args[2]);
+            }
+            else {
+                W.Write(args[1], args[2],args[3]);
+            }
+        }
+      
+        else if(args[0].equals("read-message")) {
+            R.Read_Message(args[1]);
+        }
+        else {
+            System.exit(0);
+        }
+   
+    }
 }
