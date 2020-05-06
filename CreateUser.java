dc158fd03a4a242d6c7e3e6bc61dcc6f89548bc8
endrit <58190531+endritbalaj@users.noreply.github.com>
2020-05-06 21:46:25 +0200

    Update CreateUser.java

--- CreateUser.java
+++ CreateUser.java
@@ -1,47 +1,129 @@
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.beans.XMLEncoder;
+import java.io.*;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.interfaces.RSAKey;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.nio.file.*;
+import static java.nio.file.StandardCopyOption.*;
+import java.net.*;
 
-public class CreateUser {
+public class Create {
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
 
-	private KeyPairGenerator keyGen;
-	private KeyPair pair;
-	private PrivateKey privateKey;
-	private PublicKey publicKey;
+    private static String Path = "keys\\\\";
 
-	public CreateUser(int keylength) throws NoSuchAlgorithmException, NoSuchProviderException {
-		this.keyGen = KeyPairGenerator.getInstance("RSA");
-		this.keyGen.initialize(keylength);
-	}
+    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+    KeyPair keyPair = keyPairGen.genKeyPair();
+    RSAPrivateCrtKey privKey = (RSAPrivateCrtKey) keyPair.getPrivate();
+    RSAPublicKey pubKey = (RSAPublicKey) keyPair.getPublic();
+	
+	Base64.Encoder encoder = Base64.getEncoder();
 
-	public void createKeys() {
-		this.pair = this.keyGen.generateKeyPair();
-		this.privateKey = pair.getPrivate();
-		this.publicKey = pair.getPublic();
-	}
+  
 
-	public PrivateKey getPrivateKey() {
-		return this.privateKey;
-	}
+    BigInteger n = ((RSAKey) privKey).getModulus();
+    BigInteger e = ((RSAPrivateCrtKey) privKey).getPublicExponent();
+    BigInteger p = ((RSAPrivateCrtKey) privKey).getPrimeP();
+    BigInteger q = ((RSAPrivateCrtKey) privKey).getPrimeQ();
+    BigInteger dp = ((RSAPrivateCrtKey) privKey).getPrimeExponentP();
+    BigInteger dq = ((RSAPrivateCrtKey) privKey).getPrimeExponentQ();
+    BigInteger inverseQ = ((RSAPrivateCrtKey) privKey).getCrtCoefficient();
+    BigInteger d = ((RSAPrivateKey) privKey).getPrivateExponent();
 
-	public PublicKey getPublicKey() {
-		return this.publicKey;
-	}
+    String N = encoder.encodeToString(n.toByteArray());
+	String E = encoder.encodeToString(e.toByteArray());
+	String P = encoder.encodeToString(p.toByteArray());
+	String Q = encoder.encodeToString(q.toByteArray());
+	String DP = encoder.encodeToString(dp.toByteArray());
+	String DQ = encoder.encodeToString(dq.toByteArray());
+	String INVERSEQ = encoder.encodeToString(inverseQ.toByteArray());
+	String D = encoder.encodeToString(d.toByteArray());
 
-	public void writeToFile(String path, byte[] key) throws IOException {
-		File f = new File(path);
-		f.getParentFile().mkdirs();
+        
 
-		FileOutputStream fos = new FileOutputStream(f);
-		fos.write(key);
-		fos.flush();
-		fos.close();
-	}
+    public Create() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024);
+        KeyPair pair = keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    public void writeToFile(String path, byte[] key) throws IOException {
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(key);
+        fos.flush();
+        fos.close();
+    }
+
+    private static void copyFile(File source, File destinacioni) throws IOException {
+    Files.copy(source.toPath(), destinacioni.toPath());
+}
+
+    public static Boolean FileExists(String user, String path, String type){
+        File tempFile = new File("" + path + user + type + "");
+        boolean exists = tempFile.exists();
+        return exists;
+    }   
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+   
+
+	public void CreateUser(String name) throws IOException {		    
+			Boolean existsPrivat = FileExists(name, Path, ".xml");
+			Boolean existsPublik = FileExists(name, Path, ".pub.xml");
+			if(!(existsPrivat && existsPublik)){
+		    	Person celsiPrivat = new Person(N,E,P,Q,DP,DQ,INVERSEQ,D);
+		        FileOutputStream ruajCelsinPrivat = new FileOutputStream(new File("keys\\\\"
+		        																	+ name +".xml"));
+				XMLEncoder enkoderiCelsitPrivat = new XMLEncoder(ruajCelsinPrivat);
+				enkoderiCelsitPrivat.writeObject(celsiPrivat);
+				enkoderiCelsitPrivat.close();
+				ruajCelsinPrivat.close();
+				
+		        Person celsiPublik = new Person(N,E);
+				FileOutputStream ruajCelsinPublik = new FileOutputStream(new File("keys\\\\"
+																		+ name +".pub.xml"));
+				XMLEncoder enkoderiCelsitPublik = new XMLEncoder(ruajCelsinPublik);
+				enkoderiCelsitPublik.writeObject(celsiPublik);
+				enkoderiCelsitPublik.close();
+				ruajCelsinPublik.close();
+				
+				System.out.println("Eshte krijuar celesi privat 'keys/" + name + ".xml'");
+				System.out.println("Eshte krijuar celesi publik 'keys/" + name + ".pub.xml'");
+			}
+			else{
+				System.out.print("Gabim: Celesi '" + name + "' ekziston paraprakisht.");
+			}
+		}
+	
+
+
+
+
 
 }
