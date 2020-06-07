998d942c17dc95c7cddf301a23156aa7378b63b6
endrit <58190531+endritbalaj@users.noreply.github.com>
2020-06-07 13:56:10 +0200

    Update UserLog.java

--- UserLog.java
+++ UserLog.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
@@ -21,12 +22,14 @@ public class UserLog {
 				
 	            Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3307/eclipse","root","balajendrit");
 
-	            PreparedStatement Pstatement=connection.prepareStatement("insert into student values(?,?,?)");
+	            PreparedStatement Pstatement=connection.prepareStatement("insert into student values(?,?,?,?,?)");
 
 	            Pstatement.setString(1,name);
 	            Pstatement.setString(2,encodePass(pasi1));
 	            Pstatement.setString(3,encodePass(pasi2));
-
+	            Pstatement.setString(4,null);
+	            Pstatement.setString(5,null);
+	            
 	            if(pasi1.equalsIgnoreCase(pasi2))
 	            {
 
@@ -79,17 +82,65 @@ public class UserLog {
 	        if(rs.next()) { 
 		         String str1 = rs.getString("USERNAME");
 		         String str2 = rs.getString("PASSWRD");
+
 		         if(str1.equalsIgnoreCase(name))
 		         {
 		        	 if(str2.equalsIgnoreCase(encodePass(UserPasword)))
-		        	 System.out.println("success");
+		        	 {
+				         String str3 = TokenGenerator.generateToken();
+
+		        		 String query = "update student set the_token = ? where username = ?";
+		        		 String query1 = "update student set ontime = ? where username = ?";
+		        		 
+		        	      PreparedStatement preparedStmt = connection.prepareStatement(query);
+		        	      preparedStmt.setString(1, str3);
+		        	      preparedStmt.setString(2,name);
+		        	      
+		        	      PreparedStatement preparedStmt1 = connection.prepareStatement(query1);
+		        	      preparedStmt1.setLong(1, TokenGenerator.activeTime());
+		        	      preparedStmt1.setString(2,name);
+		        	      
+		        	      
+		        	      preparedStmt.executeUpdate();
+		        	      preparedStmt1.executeUpdate();
+		        	      
+		 	            System.out.println(str3);
+		        	 }
 		        	 else {
-						System.out.println("fail");
+						System.out.println("Shfrytezuesi ose fjalekalimi i gabuar.");
 					}
 		         }
 	        }
         }   	
 	} 
+	
+	public static void getStatus(String token) throws SQLException {
+		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3307/eclipse","root","balajendrit");
+        Statement st = connection.createStatement();
+        String sql = ("SELECT * FROM student;");
+        ResultSet rs = st.executeQuery(sql);
+        for(int i=0;i<50;i++)
+        {
+	        if(rs.next()) { 
+	        	if(rs.getString("THE_TOKEN")==null)
+	        		continue;
+	        	else {
+	        		String str1 = rs.getString("THE_TOKEN");
+	        		long str3 = rs.getLong("ONTIME");
+			         if (str1.equals(token)) {
+			        	 String str2 = rs.getString("USERNAME");
+			        	 System.out.println("User: "+str2);
+			        	 System.out.println("Valid: "+TokenGenerator.YesNo(str3));
+			        	 System.out.println("Skadimi: " + TokenGenerator.isValid(str3));
+					} else {
+						continue;
+					}
+				}
+	        	
+		         
+	        }
+        }
+	}
 	public static String encodePass(String name) throws NoSuchAlgorithmException, IOException {
 		
 		String passwordToHash = name;
