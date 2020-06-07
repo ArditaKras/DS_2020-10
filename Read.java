bb5edee95cbffc4b95cc55b827117c75dbf15868
ArditaKras <58491146+ArditaKras@users.noreply.github.com>
2020-06-07 17:45:58 +0200

    Update Read.java

--- Read.java
+++ Read.java
@@ -8,6 +8,11 @@ import java.security.interfaces.RSAKey;
 import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -73,8 +78,30 @@ public class Read {
 			     	System.out.println("Gabim: Celesi privat 'keys/" + Marrsi +".xml' nuk ekziston");
 			    }
 			}
+	public static void ReturnMessage1(String marrsi, String mesazhi, String derguesi, String nenshkrimi) throws UnsupportedEncodingException, IOException, SQLException{
+	    byte[] dekodimiCelsit = Base64.getDecoder().decode(marrsi);
+        String Marrsi = new String(dekodimiCelsit, StandardCharsets.UTF_8.name());	
+        byte[] dekodimiMesazhit = Base64.getDecoder().decode(mesazhi);
+        String Mesazhi = new String(dekodimiMesazhit, StandardCharsets.UTF_8.name());
+        byte[] dekodimiCelsit1 = Base64.getDecoder().decode(derguesi);
+        String Derguesi = new String(dekodimiCelsit1, StandardCharsets.UTF_8.name());	
+        byte[] dekodimiMesazhit1 = Base64.getDecoder().decode(nenshkrimi);
+        String Nenshkrimi = new String(dekodimiMesazhit1, StandardCharsets.UTF_8.name());
+        
+        Boolean exist = FileExists(Marrsi, Path, ".xml");
+    if(exist) {
+        System.out.println("Marresi: " + Marrsi);
+        System.out.println("Mesazhi: " + Mesazhi);
+        System.out.println("Derguesi: " + Derguesi);
+        System.out.println("Nenshkrimi: ");
+        validate(Nenshkrimi);
+     	}
+    else{
+     	System.out.println("Gabim: Celesi privat 'keys/" + Marrsi +".xml' nuk ekziston");
+    }
+}
 
-			 public void Read_Message(String encryptedMessage) throws UnsupportedEncodingException, IOException {
+	 public void Read_Message(String encryptedMessage) throws UnsupportedEncodingException, IOException {
 			if(!encryptedMessage.endsWith(".txt")){
 					String marrsi = encryptedMessage.split("\\.", 0)[0];
 		            String mesazhi = encryptedMessage.split("\\.", 0)[3];
@@ -88,8 +115,49 @@ public class Read {
 		            String mesazhi = contents.split("\\.", 0)[3];
 		            ReturnMessage(marrsi,mesazhi);
 			}}
-
-	
-
-
+			 
+	 public void Read_Message1(String encryptedMessage) throws UnsupportedEncodingException, IOException, SQLException {
+			if(!encryptedMessage.endsWith(".txt")){
+					String marrsi = encryptedMessage.split("\\.", 0)[0];
+					String mesazhi = encryptedMessage.split("\\.", 0)[3];
+					String derguesi = encryptedMessage.split("\\.", 0)[4];
+		            String nenshkrimi = encryptedMessage.split("\\.", 0)[5];
+		            ReturnMessage1(marrsi, mesazhi, derguesi, nenshkrimi);
+			}
+			else{ 
+					String contents = Files.lines(Paths.get("keys\\\\" +
+								encryptedMessage)).collect(Collectors.joining("\n"));
+					contents = contents.split("<string>")[1].split("</string>")[0];
+					String marrsi = contents.split("\\.", 0)[0];
+		            String mesazhi = contents.split("\\.", 0)[3];
+					String derguesi = contents.split("\\.", 0)[4];
+		            String nenshkrimi = contents.split("\\.", 0)[5];
+		            ReturnMessage1(marrsi, mesazhi, derguesi, nenshkrimi);
+			}}			
+			 
+	 public static void validate(String token) throws SQLException {
+		 
+			Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3307/eclipse","root","balajendrit");
+	        Statement st = connection.createStatement();
+	        String sql = ("SELECT * FROM student;");
+	        ResultSet rs = st.executeQuery(sql);
+	        for(int i=0;i<50;i++)
+	        {
+		        if(rs.next()) { 
+		        	if(rs.getString("THE_TOKEN")==null)
+		        		continue;
+		        	else {
+		        		String str1 = rs.getString("THE_TOKEN");
+		        		long str3 = rs.getLong("ONTIME");
+				         if (str1.equals(token)) {
+				        	 
+				        	String isval = TokenGenerator.YesNo(str3);
+				        	System.out.println(isval);
+						} else {
+							continue;
+						}
+					}      
+		        }
+	        }  
+		}	 
 }
